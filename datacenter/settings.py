e963675a548ec6049930fee113d25a199a65d11b
jimmy-shian <jimmy910824@gmail.com>
2022-05-25 21:06:32 +0800

    password

--- datacenter/settings.py
+++ datacenter/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*2fqgn547%8g+48pjr!z_+&gusk2+@hi)h#ra^9#6ra&4r1#ok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
